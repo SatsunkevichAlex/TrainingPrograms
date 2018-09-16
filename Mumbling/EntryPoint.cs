0df63e5f71dcf2c13570ce74c05652c0765c78fb
Alex <sasha.satsunkevich@gmail.com>
2018-09-16 23:29:01 +0300

    start but did not finish training progreamm

---
+++ Mumbling/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Mumbling
+{
+    class EntyPoint
+    {
+        static void Main(string[] args)
+        {
+            //char y = 'b';
+            //int x = (int)y;
+            //Console.WriteLine(x);
+
+            Console.WriteLine(Accum("123"));
+        }
+
+        public static string Accum(string sequance)
+        {
+            StringBuilder resultSequance = new StringBuilder("");
+
+            for (int i = 0; i < sequance.Length; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+                    resultSequance.Append(sequance[i]);
+                }
+
+                resultSequance.Append("-");
+            }
+
+            return resultSequance.R.ToString();
+        }
+    }
+}
