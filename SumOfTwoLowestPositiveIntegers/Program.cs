8ba9be4fe5f963c3aaaa8a29c71d8dfc29d656f4
Aliaksandr_Satsunkevich <Aliaksandr_Satsunkevich@epam.com>
2018-09-17 14:01:04 +0300

    begin with sum of two lowest positive int

---
+++ SumOfTwoLowestPositiveIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 integers. No floats or empty arrays will be passed.
+//For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+//[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+using System;
+using System.Linq;
+
+namespace SumOfTwoLowestPositiveIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 2, 3, 5 };
+        }
+    }
+
+    public static int SumTwoSmallestNumbers(int[] numbers)
+    {
+        if (numbers.Length < 4
+        || numbers == null)
+        {
+            throw new Exception("Invalid input");
+        }
+        var result = numbers.OrderBy();
+
+        return 0;
+    }
+}
