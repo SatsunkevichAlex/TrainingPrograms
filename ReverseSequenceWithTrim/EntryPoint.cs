f3883c12bf427b1d13c624be3ac5b50880b77e5a
Aliaksandr_Satsunkevich <Aliaksandr_Satsunkevich@epam.com>
2018-09-26 09:24:34 +0300

    add reverse sequance with omitting non-alphabet characters

---
+++ ReverseSequenceWithTrim/EntryPoint.cs
@@ -0,0 +1,29 @@
+﻿//Given a string str, reverse it omitting all non-alphabetic characters.
+
+using System;
+using System.Linq;
+
+namespace ReverseSequenceWithTrim
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseLetter("qwert123"));
+        }
+
+        public static string ReverseLetter(string str)
+        {
+            const string Alphabet = "QqWwEeRrTtYyUuIiOoPpAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMm";
+            var sortedStr = str.Where(c => Alphabet.Contains(c));
+            string result = "";
+            
+            foreach(char c in sortedStr.Reverse())
+            {
+                result += c;
+            }
+
+            return result;
+        }
+    }
+}
