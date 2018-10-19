dfbd84775586d009222dd57066f17ddb188351f7
Aliaksandr_Satsunkevich <Aliaksandr_Satsunkevich@epam.com>
2018-10-19 19:11:55 +0300

    add program with calculate factorial (better use instead of recursion for big digits (>50))

---
+++ CountCharadtersInString/Program.cs
@@ -0,0 +1,28 @@
+﻿//The main idea is to count all the occuring characters(UTF-8) in string. If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+//What if the string is empty? Then the result should be empty object literal { }
+//For C#: Use a Dictionary<char, int> for this kata!
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharadtersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var group in Count("aassddff"))
+            {
+                Console.WriteLine(group.Key + " " + group.Value);
+            }
+        }
+
+        public static Dictionary<char, int> Count(string str)
+        {
+            return str.ToCharArray()
+                      .GroupBy(c => c)
+                      .ToDictionary(k => k.Key, k => k.Count());
+        }
+    }
+}
